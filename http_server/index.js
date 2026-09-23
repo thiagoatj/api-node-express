8010f644b6028ce3d730ef0fd596c4bc34582c6e
Thiago <thiago.atj2006@gmail.com>
2026-09-22 22:30:23 -0300

    Corrige pasta http_server no repositorio

---
+++ http_server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+let PORTA = 3000;
+const server = http.createServer(
+(req,res)=> {
+console.log('URL: ' + req.url);
+console.log('IP REMOTO: ' + req.connection.remoteIP);
+console.log('PORTA REMOTA: ' + req.connection.remotePort);
+res.end('<h1>BEM-VINDO AO MEU SITE!</h1>');
+}
+);
+server.listen(PORTA);
+console.log('*** Olá, Startando Servidor Web com Node ***');
